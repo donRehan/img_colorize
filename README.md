131c5fb200b2f7daf4aac931798caab73e35acba
BluBird4 <alhussienrehan@hotmail.com>
2023-04-15 02:50:07 +0200

    Added colorization_deploy_v2

---
+++ README.md
@@ -0,0 +1,23 @@
+This is a training project in Computer vision to colozie a given image. Currently it works by adding the image in the **images** folder.
+Output is produced in the **Output** Folder.
+
+SCREENSHOTS::
+
+
+*TODO 
+-[ ] Allow users to select a different directory.
+-[ ] Add fixing images that are toren or messed up.
+-[ ] Sharpen images.
+-[ ] Make the terminal gui better.
+
+Motivation ::
+
+
+Credits :: 
+
+@richzhang :: https://github.com/richzhang/colorization/tree/caffe/colorization/models
+NeuralNine youtube account :: https://www.youtube.com/watch?v=oNjQpq8QuAo&t=541s
+https://github.com/PySimpleGUI/PySimpleGUI-Photo-Colorizer/blob/master/PySimpleGUI_Colorizer.py
+
+You will need the following pretained data and add it to models file
+https://www.dropbox.com/s/dx0qvhhp5hbcx7z/colorization_release_v2.caffemodel?dl=1
--- main.py
+++ main.py
@@ -1,2 +1,39 @@
+#! /usr/local/bin/python3
 import numpy as np
+import os
 import cv2
+
+
+"""
+Inspired by https://www.youtube.com/watch?v=oNjQpq8QuAo&t=588s
+Paths to the files
+
+I want to have a menu that shows the available images names in the stdoutput and make the user choose one of them
+"""
+
+
+""" Show the images in the directory """
+images = os.listdir("images")
+print(images)
+
+
+""" Make the user select the image by its index of that list """
+
+print("Accessing elements is done by thier index , starting from 0")
+print(" ")
+image_index = int(input("Enter the index of the image: "))
+image_path = "images/" + images[image_index]
+
+prototxt_path = "models/colorization_deploy_v2.prototxt"
+model_path = "models/colorization_release_v2.caffemodel"
+kernel_path = "models/pts_in_hull.npy"
+image_path = "images/" + input("Enter the name of the image: ")
+
+"""
+# Load the image
+#image = cv2.imread(image_path)
+
+#
+#net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+#points = np.load(kernel_path)
+"""
